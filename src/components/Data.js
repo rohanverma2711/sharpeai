c1d739f4802486bacd10fa78891ada8d1656dea5
Rohan Kumar <imrohanverma27@gmail.com>
2023-12-02 20:14:26 +0530

    Added a Loader

--- src/components/Data.js
+++ src/components/Data.js
@@ -2,26 +2,16 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { PieChart, Pie, Cell, ResponsiveContainer } from 'recharts';
 import { useNavigate } from 'react-router-dom';
+import Loader from './Loader';
 
 
 const Data = () => {
   const [posts, setPosts] = useState([]);
   const navigate = useNavigate();
   const [filteredPosts, setFilteredPosts] = useState([]);
+  const [loading, setLoading] = useState();
 
-  useEffect(() => {
-    // Fetch data from the API
-    axios.get('https://jsonplaceholder.typicode.com/posts')
-      .then((response) => {
-        setPosts(response.data);
-        filterPosts(response.data);
-      })
-      .catch((error) => {
-        console.error('Error fetching data:', error);
-      });
-  }, []);
 
-console.log(posts,filteredPosts)
   const filterPosts = (allPosts) => {
     // Filter posts with user ID 1
     const filtered = allPosts.filter((post) => post.userId === 1);
@@ -29,12 +19,36 @@ console.log(posts,filteredPosts)
   };
 
 
+  useEffect(() => {
+    // Fetch data from the API
+    setLoading(true);
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then((response) => {
+        setPosts(response.data);
+        filterPosts(response.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      })
+      .finally(() => {
+        // Hide loader when data fetching is complete (success or failure)
+        setLoading(false);
+      });
+      
+    
+  }, []);
+
+console.log(posts,filteredPosts)
+  
+
   const dataForPieChart = [
     { name: 'User ID 1 Posts', value: filteredPosts.length },
     { name: 'Other Posts', value: posts.length - filteredPosts.length },
   ];
 
   return (
+<>
+   
     <div className='mb-10'>
        <div className=" hidden sm:block w-4/5 my-4 py-4 rounded-xlg border-1  shadow-navShadow mx-auto mb-10">
         <div className="flex flex-row justify-between mx-20">
@@ -63,56 +77,67 @@ console.log(posts,filteredPosts)
           </div>
         </div>
       </div>
-      <h1 className='font-monste font-bold  text-4xl text-center mb-10'>Posts Visualization</h1>
+      {loading && <Loader className = "text-center border-2 border-black"/>}
+      {!loading && (
+        <div>
 
-      {/* Table */}
-      <table className='w-full text-lg  text-gray-500 m-10'>
-        <thead className='text-lg text-gray-700 uppercase bg-gray-50'>
-          <tr>
-            <th>ID</th>
-            <th>Title</th>
-            <th>Body</th>
-          </tr>
-        </thead>
-        <tbody>
-          {filteredPosts.map((post) => (
-            <tr key={post.id}>
-              <td>{post.id}</td>
-              <td>{post.title}</td>
-              <td>{post.body}</td>
+      
+        <h1 className='font-monste font-bold  text-4xl text-center mb-10'>Posts Visualization</h1>
+  
+        {/* Table */}
+        <div className='w-full overflow-x-auto'>
+        <table className='w-full text-lg  text-gray-500 m-10'>
+          <thead className='text-lg text-gray-700 uppercase bg-gray-50'>
+            <tr>
+              <th>ID</th>
+              <th>Title</th>
+              <th>Body</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-
-      {/* Pie Chart */}
-      <h1 className='font-monste font-bold  text-4xl text-center my-18'>Pie Chart</h1>
-      <div className='flex  justify-center'>
-      
-
-      
-      <div className='h-80 w-80'>
-        <ResponsiveContainer>
-          <PieChart>
-            <Pie
-              dataKey="value"
-              isAnimationActive={false}
-              data={dataForPieChart}
-              cx="50%"
-              cy="50%"
-              outerRadius={80}
-              fill="#8884d8"
-              label
-            >
-              {dataForPieChart.map((entry, index) => (
-                <Cell key={`cell-${index}`} fill={`#${index === 0 ? 'FF6384' : '36A2EB'}`} />
-              ))}
-            </Pie>
-          </PieChart>
-        </ResponsiveContainer>
+          </thead>
+          <tbody>
+            {filteredPosts.map((post) => (
+              <tr key={post.id}>
+                <td>{post.id}</td>
+                <td>{post.title}</td>
+                <td>{post.body}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
         </div>
-        </div>
+  
+        {/* Pie Chart */}
+        <h1 className='font-monste font-bold  text-4xl text-center my-18'>Pie Chart</h1>
+        <div className='flex  justify-center'>
+        
+  
+        
+        <div className='h-80 w-80'>
+          <ResponsiveContainer>
+            <PieChart>
+              <Pie
+                dataKey="value"
+                isAnimationActive={false}
+                data={dataForPieChart}
+                cx="50%"
+                cy="50%"
+                outerRadius={80}
+                fill="#8884d8"
+                label
+              >
+                {dataForPieChart.map((entry, index) => (
+                  <Cell key={`cell-${index}`} fill={`#${index === 0 ? 'FF6384' : '36A2EB'}`} />
+                ))}
+              </Pie>
+            </PieChart>
+          </ResponsiveContainer>
+          </div>
+          </div>
+          </div>
+      )}
+      
     </div>
+    </>
   );
 };
 
